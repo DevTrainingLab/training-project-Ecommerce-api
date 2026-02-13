a0bdde04c0b608b3ad598ca48a83abd9a6b1c998
40moaz <amoaz14109@gmail.com>
2026-02-13 05:20:39 +0200

    first push

---
+++ src/models/product.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    reviewsCount: {
+      type: Number,
+      default: 0,
+    },
+    images: [String],
+    colors: [
+      {
+        name: String,
+        hex: String,
+      },
+    ],
+    sizes: [
+      {
+        size: String,
+        stock: Number,
+      },
+    ],
+    totalStock: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
