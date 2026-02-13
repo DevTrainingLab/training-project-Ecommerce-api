2d0953404f9b21638c69add9a747d19da6e3280c
40moaz <amoaz14109@gmail.com>
2026-02-13 06:32:22 +0200

    server

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const productRoutes = require("./routes/product.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api", userRoutes);
 
 module.exports = app;
