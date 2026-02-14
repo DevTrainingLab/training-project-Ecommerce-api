4f92b25eb2cb8f8e360c2de57a2d6e89cd3ac3ee
40moaz <amoaz14109@gmail.com>
2026-02-14 22:39:18 +0200

    fix: update order routes to use destructured controller methods

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createOrder,
-  getMyOrders,
-  getOrderById,
-  getAllOrders,
-} = require("../controllers/order.controller");
+const orderController = require("../controllers/order.controller");
 
 const { protect, admin } = require("../middlewares/auth.middleware");
 
-router.post("/", protect, createOrder);
-router.get("/my", protect, getMyOrders);
-router.get("/:id", protect, getOrderById);
-router.get("/", protect, admin, getAllOrders);
+router.post("/", protect, orderController.createOrder);
+router.get("/my", protect, orderController.getMyOrders);
+router.get("/:id", protect, orderController.getOrderById);
+router.get("/", protect, admin, orderController.getAllOrders);
 
 module.exports = router;
