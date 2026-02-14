e45b7752f81c443a66edc48470bab88c4f73e72f
40moaz <amoaz14109@gmail.com>
2026-02-14 21:49:50 +0200

    feat: update login method to include password in user query for authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,9 +44,9 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("+password");
     if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({
+      res.status(200).json({
         _id: user._id,
         firstName: user.firstName,
         lastName: user.lastName,
