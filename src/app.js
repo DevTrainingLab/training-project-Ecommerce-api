bce7734c3caf7e87fd4cee65e5cdaa657b22a3f4
40moaz <amoaz14109@gmail.com>
2026-02-16 16:49:47 +0200

    tt

--- src/app.js
+++ src/app.js
@@ -6,22 +6,8 @@ const userRoutes = require("./routes/user.routes");
 const cartRoutes = require("./routes/cart.routes");
 const orderRoutes = require("./routes/order.routes");
 
-const app = express();
-
 app.use(
-  cors({
-origin: function (origin, callback) {
-  if (!origin) return callback(null, true);
-
-  if (/^http:\/\/localhost:\d+$/.test(origin)) {
-    return callback(null, true);
-  }
-
-  callback(new Error("Not allowed by CORS"));
-},
-
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.json());
