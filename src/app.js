e2bbb11649801fced1f83411a0d45a08bdd15874
40moaz <amoaz14109@gmail.com>
2026-02-16 16:58:44 +0200

    kj

--- src/app.js
+++ src/app.js
@@ -5,11 +5,9 @@ const productRoutes = require("./routes/product.routes");
 const userRoutes = require("./routes/user.routes");
 const cartRoutes = require("./routes/cart.routes");
 const orderRoutes = require("./routes/order.routes");
+const app = express();
 
-app.use(
-  cors()
-);
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/cart", cartRoutes);
