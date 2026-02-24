7ae9ccdcfbe43e861f61826b2fb12e739df2b420
40moaz <amoaz14109@gmail.com>
2026-02-24 23:29:39 +0200

    ح

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -73,7 +73,7 @@ const getOrderById = async (req, res) => {
 };
 const getAllOrders = async (req, res) => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().populate("user", "firstName email");
 
     res.json(orders);
   } catch (err) {
