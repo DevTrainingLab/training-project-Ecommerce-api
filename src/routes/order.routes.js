acefccf1ef8e4ec407726ea6b585d19075ffee76
40moaz <amoaz14109@gmail.com>
2026-02-26 00:57:46 +0200

    efefe

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -6,7 +6,7 @@ const { protect, admin } = require("../middlewares/auth.middleware");
 
 router.post("/", protect, orderController.createOrder);
 router.get("/my", protect, orderController.getMyOrders);
-router.get("/:id", protect, orderController.getOrderById);
+router.get("/:id", admin, protect, orderController.getOrderById);
 router.get("/", protect, admin, orderController.getAllOrders);
 
 module.exports = router;
